5b4dd162a4a9ca3144e27b0ea57485e172e90f4b
Tommaso <tommasobaccano@gmail.com>
2023-04-12 14:43:57 +0200

    Prova commit

---
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+
+//Come visto a lezione, strutturare la classe in modo che non possa essere istanziata e che presenti i seguenti metodi `static`:
+//-Somma di due numeri interi
+//- Somma di due numeri double
+//- Differenza tra due numeri interi
+//- Differenza tra due numeri double
+//- Moltiplicazione di due numeri interi
+//- Moltiplicazione di due numeri double
+//- Valore assoluto di un numero intero
+//- Valore assoluto di un numero double
+//- Minimo tra due numeri interi
+//- Minimo tra due numeri double
+//- Massimo tra due numeri interi
+//- Massimo tra due numeri double
+//Il fatto di dover scrivere lo stesso metodo per tipi di parametro diversi applica nella pratica uno dei principi di programmazione ad oggetti che abbiamo visto oggi…Quale? Scrivetelo in un commento all’interno del codice! 
+//Testate tutti i metodi della vostra classe di helper (con un esempio per ogni casistica).
+
+
+namespace csharp_calcolatrice
+{
+	public class CalcoliHelper
+	{
+		public CalcoliHelper()
+		{
+		}
+	}
+}
+
